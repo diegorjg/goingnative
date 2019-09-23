820c6bebee61e6771aa50168d0ff4169ce2fd8f3
diego <diegorjg@gmail.com>
2019-09-23 09:27:31 -0500

    first commit

---
+++ myaddon.cc
@@ -0,0 +1,13 @@
+#include <nan.h>
+
+using namespace v8;
+
+NAN_METHOD(Print) {
+
+}
+
+NAN_MODULE_INIT(Init) {
+  Nan::Set(target, Nan::New("print").ToLocalChecked(),
+      Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
+}
+NODE_MODULE(myaddon, Init)
